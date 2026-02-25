3ad925965df5fac3a74bb58381ae91653651fd35
ngan2024 <ngantran011082@gmail.com>
2026-02-25 11:08:16 +0000

    ADD STORE

---
+++ bed-2026-ex3-restAPI/step0/store.js
@@ -0,0 +1,52 @@
+//
+
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const filename = path.join(process.cwd(), 'db.json');
+
+const saveChanges = async (data) => {
+    await fs.writeFile(filename, JSON.stringify(data));
+};
+
+const readData = async () => {
+    const data = await fs.readFile(filename, 'utf-8');
+    return JSON.parse(data);
+};
+
+const getAll = readData;
+
+const getById = async (id) => {
+    const data = await readData();
+    return data.find(item => item.id ===id);
+};
+
+const create =async (item) => {
+    const data = await readData();
+    const newItem = { id: Date.now().toString(), ...item};
+    data.push(newItem);
+    await saveChanges(data);
+    return newItem;
+};
+
+const updateById = async (id, message) => {
+    const data = await readData();              //start reading data from filename, data is an array object
+    const newData = data.map(current => current.id ===id ?
+        {...current, message} : current);       //currrent object
+        await saveChanges(newData);
+    
+};
+
+const deleteId =async (id) => {
+    const data = await readData();
+    const newData = data.filter(item => item.id !== id);
+    await saveChanges(newData);
+};
+
+export {
+    getAll,
+    getById,
+    create,
+    updateById,
+    deleteId
+};
